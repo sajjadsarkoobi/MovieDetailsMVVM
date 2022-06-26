d91ad94fd437d8983e52bd944ff4bf76f9cc1618
Sajjad Sarkoobi <sajjadsarkoobi@gmail.com>
2022-06-26 12:52:55 +0300

    Initial Commit

---
+++ MovieDetailsMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDetailsMVVM
+//
+//  Created by Sajjad Sarkoobi on 26.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
