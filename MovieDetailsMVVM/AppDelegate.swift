cd8ce7fa1adf62f3647abc897e48495780d1696a
Sajjad Sarkoobi <sajjadsarkoobi@gmail.com>
2022-06-26 13:14:31 +0300

    Remove Storyboard & defining the main view controller

--- MovieDetailsMVVM/AppDelegate.swift
+++ MovieDetailsMVVM/AppDelegate.swift
@@ -15,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = MainViewController()
+        window.makeKeyAndVisible()
+        
+        self.window = window
+        
         return true
     }
 }
