10c080e2932a721532c451ed6ae2d24d96c1e23b
Sajjad Sarkoobi <sajjadsarkoobi@gmail.com>
2022-06-27 11:06:23 +0300

    add UIView Extension

---
+++ MovieDetailsMVVM/Extensions/UIViewExtensions.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewExtensions.swift
+//  MovieDetailsMVVM
+//
+//  Created by Sajjad Sarkoobi on 27.06.2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    func round( _ radious: CGFloat = 10) {
+        self.layer.cornerRadius = radious
+        self.clipsToBounds = true
+    }
+    
+    func addBorder(color: UIColor, width: CGFloat) {
+        self.layer.borderColor = color.cgColor
+        self.layer.borderWidth = width
+    }
+}
